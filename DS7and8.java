903cad3e0e4872a4e6093d09d5652db55d064570
rrk01 <ryanrizzulkarki@yahoo.com>
2022-09-29 12:24:40 -0600

    Add files via upload
    stack and q with arraylist

---
+++ DS7and8.java
@@ -0,0 +1,37 @@
+package cogenttest;
+import java.util.ArrayList;
+
+public class DS7and8 {
+	
+	public ArrayList<Integer> stack;
+	public ArrayList<Integer> queue;
+	
+	public DS7and8() {
+		stack = new ArrayList<Integer>();
+		queue = new ArrayList<Integer>();
+	}
+	
+	public void addStack(int i) {
+		stack.add(i);
+	}
+	
+	public int popStack() {
+		int a = stack.get(stack.size() - 1);
+		stack.remove(stack.size() - 1);
+		return a;
+	}
+	
+	public void addQueue(int i) {
+		queue.add(i);
+	}
+	
+	public int popQueue() {
+		int a = queue.get(0);
+		queue.remove(0);
+		return a;
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
